b6d23bb4946eeaa86fdf65ad70c34ad4de3f4382
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-03-17 10:23:01 +0530

    Questions On Searching

--- src/main/java/searcher/linearsearch/StudentLinearSearch.java
+++ src/main/java/searcher/linearsearch/StudentLinearSearch.java
@@ -1,4 +1,40 @@
 package searcher.linearsearch;
 
 public class StudentLinearSearch {
+    int rollNumber;
+    String name;
+
+    public StudentLinearSearch(int rollNumber, String name) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentLinearSearch{" + "rollNumber=" + rollNumber + ", name='" + name + '\'' + '}';
+    }
+}
+
+class Searcher {
+    public static void main(String[] args) {
+        StudentLinearSearch[] students = new StudentLinearSearch[]{new StudentLinearSearch(9, "Ram"), new StudentLinearSearch(3, "Akash"), new StudentLinearSearch(1, "Abhishek")};
+
+        int key = 3;
+        int index = search(students, key);
+        if (index != -1) {
+            System.out.println("Student found at index : " + index);
+            System.out.println("Student details: " + students[index]);
+        } else {
+            System.out.println("Student not found.");
+        }
+    }
+
+    public static int search(StudentLinearSearch[] students, int key) {
+        for (int index = 0; index < students.length; index++) {
+            if (students[index].rollNumber == key) {
+                return index;
+            }
+        }
+        return -1;
+    }
 }
